ecaef8edae0555034163b5c50a6b9185bb124502
haoren1994 <775689360@qq.com>
2020-02-26 15:24:46 +0800

    first code

---
+++ src/test/java/com/ruanshumeng/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruanshumeng.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
